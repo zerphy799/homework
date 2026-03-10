b2538d4f383dbbc09137fbeb2394cdc4ff3ca51f
zerphy799 <2692787472@qq.com>
2026-03-10 10:59:32 +0800

    This is Exercise4

--- eRyder/src/Bike.java
+++ eRyder/src/Bike.java
@@ -1,17 +1,32 @@
+
 public class Bike {
     private String bikeId;
     private int batterylevel;
     private boolean isAvailable;
     private double kmDriven;
-
-    public Bike() {
-    }
-
+    private String company_Name="hello eRyder";
+    private double base_Fare=1.0;
+    private double per_Minute_Fare=0.5;
+    private final String linked_Account;
+    private final String linked_PhoneNumber;
+    private double total_Fare;
+    private int totalUsageINMinutes;
     public Bike(int batterylevel, String bikeId, boolean isAvailable, double kmDriven) {
         this.batterylevel = batterylevel;
         this.bikeId = bikeId;
         this.isAvailable = isAvailable;
         this.kmDriven = kmDriven;
+        this.linked_Account = String.valueOf((int)(Math.random()*100000));
+        this.linked_PhoneNumber = "184"+ (int) (Math.random() * 10000000);
+    }
+
+    public Bike(int batterylevel, String bikeId, boolean isAvailable, double kmDriven,String linked_Account, String linked_PhoneNumber) {
+        this.batterylevel = batterylevel;
+        this.linked_Account = linked_Account;
+        this.linked_PhoneNumber = linked_PhoneNumber;
+        this.bikeId = bikeId;
+        this.kmDriven = kmDriven;
+        this.isAvailable = isAvailable;
     }
 
     public int getBatterylevel() {
@@ -20,7 +35,7 @@ public class Bike {
 
     public void setBatterylevel(int batterylevel) {
         if(batterylevel < 0||batterylevel > 100){
-            System.out.println("Batterylevel must be between 0 and 100");
+            System.out.println("Baterylevel must be between 0 and 100");
             return;
         }
         this.batterylevel = batterylevel;
@@ -57,7 +72,14 @@ public class Bike {
         }
     }
     public void printBikeDetails(){
-        System.out.println("ID:"+bikeId+"\nbatterylevel:"+batterylevel+"\nisavailable:"+isAvailable+"\nkmDriven:"+kmDriven+"km");
+        System.out.println("ID:"+bikeId+"\nbatterylevel:"+batterylevel+"\nisavailable:"+isAvailable+"\nkmDriven:"+kmDriven+"km"+"\ncompany:"+company_Name);
     }
-
+    public void printRideDetails(int usageInMinutes){
+        System.out.println("account:"+linked_Account+"\nphone number:"+linked_PhoneNumber+"\nID:"+bikeId
+        +"\ntotal Fare:"+calculateFare(usageInMinutes)+"￥\ntotalUsageINMinutes:"+usageInMinutes+" minutes"
+        );
+    }
+    private double calculateFare(int usageInMinutes){
+       return total_Fare=base_Fare+per_Minute_Fare*usageInMinutes;
+    }
 }
--- eRyder/src/Run.java
+++ eRyder/src/Run.java
@@ -1,11 +1,15 @@
 public class Run {
 public static void main(String[] args) {
-    Bike bike = new Bike();
+    Bike bike = new Bike(64,"123555",true,56.2);
     bike.printBikeDetails();
-    Bike newBike = new Bike(85,"123456",true,42.2);
+    System.out.println("-------------------");
+    bike.printRideDetails(32);
+    System.out.println("------------------");
+    Bike newBike = new Bike(85,"123456",true,42.2,"411681","18351992562");
     newBike.printBikeDetails();
+    System.out.println("-----------------");
+    newBike.printRideDetails(15);
     newBike.ride();
-
 }
 
 }
